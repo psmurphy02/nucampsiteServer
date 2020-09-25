3411570675ff495a11eeeef9ebf262e0884bc17f
Patrick Murphy <psmurphy02@gmail.com>
2020-09-25 13:43:51 -0700

    Passport

--- models/user.js
+++ models/user.js
@@ -1,20 +1,24 @@
 const mongoose = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
+    // These removed when adding passportLocalMongoose:
+    // username: {
+    //     type: String,
+    //     required: true,
+    //     unique: true
+    // },
+    // password: {
+    //     type: String,
+    //     required: true
+    // },
     admin: {
         type: Boolean,
         default: false
     }
 });
+
+userSchema.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model('User', userSchema);
