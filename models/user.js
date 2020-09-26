d187ed23af6dade2c24690a65969b368381ab551
Patrick Murphy <psmurphy02@gmail.com>
2020-09-25 18:08:02 -0700

    Mongoose Population

--- models/user.js
+++ models/user.js
@@ -13,6 +13,14 @@ const userSchema = new Schema({
     //     type: String,
     //     required: true
     // },
+    firstname: {
+        type: String,
+        default: ''
+    },
+    lastname: {
+        type: String,
+        default: ''
+    },
     admin: {
         type: Boolean,
         default: false
